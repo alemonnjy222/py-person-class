d096d7a4b97de02b295d4c681e681cbe15e6b4c9
alemonnjy222 <71006485+alemonnjy222@users.noreply.github.com>
2026-05-07 08:25:36 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,11 +2,9 @@ class Person:
     people = {}
 
     def __init__(self, name: str, age: int) -> None:
-
         self.name = name
         self.age = age
         Person.people[name] = self
-
 
 def create_person_list(people: list) -> list:
     persons = [Person(person_data["name"], person_data["age"])
