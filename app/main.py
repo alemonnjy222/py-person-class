6d551754ff103233bf8173f36e3980104d0348c8
alemonnjy222 <71006485+alemonnjy222@users.noreply.github.com>
2026-05-07 08:30:57 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,16 +6,17 @@ class Person:
         self.age = age
         Person.people[name] = self
 
+
 def create_person_list(people: list) -> list:
     persons = [Person(person_data["name"], person_data["age"])
                for person_data in people]
 
     for person_data in people:
         person = Person.people[person_data["name"]]
-        
+
         if person_data.get("wife"):
             person.wife = Person.people[person_data["wife"]]
-            
+
         if person_data.get("husband"):
             person.husband = Person.people[person_data["husband"]]
 
